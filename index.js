b7dd9b7775c6ac4341be01d2f06a901c5b6b30c0
NATTA984 <79819686+NATTA984@users.noreply.github.com>
2021-03-01 10:24:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
-//const grupch1 = 'belum ada grup'; 
-//const grupch2 = 'belum ada grup' ; 
+const BotName = 'ARIZBOT☄'; 
+const whatssap = '081330091653';
+const telegram = 'https://t.me/arisrmdn'; 
+
+
+
+
+
+
 const
 {
    WAConnection,
@@ -68,7 +70,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ariz`)
 })
 
 conn.on('message-new', async(m) =>
